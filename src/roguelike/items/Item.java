4a7c5de2b422ce93f07d2884924ecdba2263a5ae
otso <otso@otso-PC>
2014-04-20 23:27:12 +0300

    new map paint method, faster

---
+++ src/roguelike/items/Item.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package roguelike.items;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author otso
+ */
+public abstract class Item {
+    
+    protected char glyph;
+    protected Color color;
+    protected int id;
+    
+}
